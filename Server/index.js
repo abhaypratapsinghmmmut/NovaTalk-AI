a6113a1a768283504c18f99550040b5998a903d0
abhaypratapsinghmmmut <abhaypratapsinghmmmut@gmail.com>
2026-05-24 23:37:09 +0530

    first commit

---
+++ Server/index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app=express();
+
+const PORT=5000;
+
+app.get('/',(req,res) =>{
+    res.json("hello from server");
+})
+
+app.listen(PORT, ()=>{
+    console.log(`server started at port ${PORT}`);
+})
