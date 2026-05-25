232eeb781ce139e754ad84931462fbcb5cfd878a
abhaypratapsinghmmmut <abhaypratapsinghmmmut@gmail.com>
2026-05-25 23:51:39 +0530

    second day commits done

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,12 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
 
 const app=express();
 
-const PORT=5000;
+const PORT=process.env.PORT;
 
 app.get('/',(req,res) =>{
     res.json("hello from server");
