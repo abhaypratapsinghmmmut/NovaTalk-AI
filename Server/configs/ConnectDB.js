3651ee8fb29944769e5929874347655dfc1b108e
abhaypratapsinghmmmut <abhaypratapsinghmmmut@gmail.com>
2026-05-26 23:57:06 +0530

    third day complete

--- Server/configs/ConnectDB.js
+++ Server/configs/ConnectDB.js
@@ -1,11 +1,16 @@
 import mongoose from 'mongoose'
 
+
 const connectDB = async () =>{
     try {
         await mongoose.connect(process.env.MONGODB_URI);
+
         console.log("Database connected")
+
     } catch (error) {
+        
         console.log("database error",error);
+        
     }
 }
 
