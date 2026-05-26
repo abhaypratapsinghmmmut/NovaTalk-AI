0e27af99d1c38dd5d4dddc7082e66d1c9be491a4
abhaypratapsinghmmmut <abhaypratapsinghmmmut@gmail.com>
2026-05-27 00:35:26 +0530

    fourth day

--- Server/index.js
+++ Server/index.js
@@ -1,17 +1,25 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './configs/ConnectDB.js';
+import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 
 const app=express();
 
+app.use(express.json());
+app.use(cookieParser());
+
+
 const PORT=process.env.PORT;
 
 app.get('/',(req,res) =>{
     res.json("hello from server");
 })
+
+app.use('/api/auth', authRouter)
 
 app.listen(PORT, ()=>{
     console.log(`server started at port ${PORT}`);
