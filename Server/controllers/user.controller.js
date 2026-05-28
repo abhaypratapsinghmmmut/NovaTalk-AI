d6fbf5f06c2a5b1670ed74feb5203d47061f73f6
abhaypratapsinghmmmut <abhaypratapsinghmmmut@gmail.com>
2026-05-28 23:29:13 +0530

    user authentication completed

---
+++ Server/controllers/user.controller.js
@@ -0,0 +1,19 @@
+import User from '../model/user.model.js'
+
+
+
+export const getCurrentUser = async (req,res) => {
+    try {
+        
+        const user = await User.findById(req.userId);
+
+        if(!user){
+            return res.status(404).json({message:"failed to get current user"});
+        }
+
+        return res.status(200).json(user);
+
+    } catch (error) {
+        return res.status(500).json({message:`get current user error ${error}`});
+    }
+}
--- Server/index.js
+++ Server/index.js
@@ -4,6 +4,7 @@ import connectDB from './configs/ConnectDB.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors'
+import userRouter from './routes/user.route.js';
 
 
 dotenv.config();
@@ -25,7 +26,8 @@ app.get('/',(req,res) =>{
     res.json("hello from server");
 })
 
-app.use('/api/auth', authRouter)
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
 
 app.listen(PORT, ()=>{
     console.log(`server started at port ${PORT}`);
