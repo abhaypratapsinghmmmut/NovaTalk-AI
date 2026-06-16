37d5cf2de65b875e257d2399f4cb2d7a7caaa231
abhaypratapsinghmmmut <abhaypratapsinghmmmut@gmail.com>
2026-06-16 18:29:03 +0530

    full css of assistant done

--- Client/public/assistant.js
+++ Client/public/assistant.js
@@ -24,7 +24,92 @@
 
     popup.className = `nova-popup theme-${theme}`
 
-    // popup.innerHTML = 
+    popup.innerHTML = `
+    
+    <div class="nova-overlay"></div>
+
+    <div class="nova-content">
+
+    <div class="nova-top">
+
+        <div class="nova-orb-wrap">
+
+            <div class="nova-orb-glow"></div>
+
+            <div class="nova-orb"></div>
+
+        </div>
+
+        <h2 class="nova-title">
+            Hello! I'm Nova AI
+        </h2>
+
+        <p class="nova-sub">
+        Your smart voice assistant.
+        <br/>
+        Ask anything about your website.
+        </p>
+
+        <div class="nova-status">
+            Tap button to Speak
+        </div>
+
+        <div class="nova-wave">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+
+        <!-- User Text -->
+        <div class="nova-user-text"></div>
+
+        <!-- AI Text -->
+        <div class="nova-ai-text"></div>
+            
+    </div>
+
+    <div class="nova-bottom">
+        <button class="nova-mic">
+            <img src="http://localhost:5173/mic.svg" alt="mic" class="nova-mic-icon" />
+        </button>
+    </div>
+
+    </div>
+
+    `;
+
+    document.body.appendChild(popup);
+
+    //floating button
+
+    const button = document.createElement("button");
+
+    button.className = `nova-btn theme-${theme}`
+
+    button.innerHTML = `
+                        <img
+                        src="http://localhost:5173/logo.png"
+                        alt="logo"
+                        />
+                        `;
+
+
+    document.body.appendChild(button);
+
+
+    //toggle popup
+
+    let open = false;
+
+    button.onclick = () =>{
+        open = !open;
+
+        popup.style.display = open ? "flex" : "none";
+    }
+
 
 
 })();
