5bd5f946f303ab570be910b2c7bc82964e6d3f39
abhaypratapsinghmmmut <166241880+abhaypratapsinghmmmut@users.noreply.github.com>
2026-06-20 12:49:48 +0530

    Update auth.controller.js

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -19,9 +19,9 @@ export const googleAuth = async (req,res) => {
 
         const token = await getToken(user._id);
         res.cookie("token" , token , {
-            httpOnly:true,
-            secure:false,
-            sameSite:"strict",
+            httpOnly:false,
+            secure:true,
+            sameSite:"none",
             maxAge:7*24*60*60*1000
         })
 
@@ -35,10 +35,10 @@ export const googleAuth = async (req,res) => {
 export const logOut = async (req,res) => {
     try {
         await res.clearCookie("token" , {
-            httpOnly:true,
-            secure:false,
+            httpOnly:false,
+            secure:true,
             
-            sameSite:"strict"
+            sameSite:"none"
         })
 
         return res.status(200).json({message:"logout sucessfully"});
